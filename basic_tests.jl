73b5a2b68309cd7c3f89dc893c7fb8b019a90707
David Lichtenberg <david@getperka.com>
2014-06-27 10:12:00 -0400

    can export tests

--- basic_tests.jl
+++ basic_tests.jl
@@ -1,8 +1,7 @@
-using JulTest
-import JulTest: jt, runtests
+import JulTest: @jt, runtests
 
 @jt begin "One plus one equals two"
-  @assert (1 + 1) == 2
+  @assert (1 + 2) == 2
 end
 
 @jt begin "verify x and y end up the same"
