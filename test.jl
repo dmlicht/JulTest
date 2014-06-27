908a27c5acb6b29f5a4f295e45db404c827b6d9c
David Lichtenberg <david@getperka.com>
2014-06-27 09:05:12 -0400

    check for exceptions

--- test.jl
+++ test.jl
@@ -2,8 +2,11 @@ using Base.Test
 
 macro jt(ex)
   description = ex.args[2]
-  println(ex.args[2])
-  println(ex.args[4])
+  try
+    $ex
+  catch error
+    println(error)
+  end
 end
 
 @jt begin "One plus one equals two"
