69b6a084afab680d7588dd4e3c6a6b800ed0b3e8
David Lichtenberg <david@getperka.com>
2014-06-29 14:06:40 -0400

    fixed paths so runner works

--- src/discover_tests.jl
+++ src/discover_tests.jl
@@ -1,11 +1,15 @@
+push!(LOAD_PATH, "src")
 import JulTest: runtests
 
 function collect()
   test_dir = "test/"
   test_dir_exists::Bool = isdir(test_dir)
-
-  filenames::Array{String} = readdir(test_dir)
-  map(x -> include("$test_dir/$x"), filenames)
+  if test_dir_exists
+    filenames::Array{String} = readdir(test_dir)
+    map(x -> include("../$test_dir$x"), filenames) #figure out how paths work better
+  else
+    println("testdir not found")
+  end
 end
 
 collect()
